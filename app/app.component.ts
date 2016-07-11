fbbc82864ae1c3faefe3c059ac5233fe6dfa9ff0
ishli <y.valyalschikov@rian.ru>
2016-07-11 12:39:24 +0300

    init

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'pm-app',
+    template: `
+    <div>
+        Hello
+     </div>
+     `
+})
+
+export class AppComponent {
+}
---
+++ app/main.ts
@@ -0,0 +1,9 @@
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+// Our main component
+import { AppComponent } from './app.component';
+
+// Our main routes
+
+
+bootstrap(AppComponent);
